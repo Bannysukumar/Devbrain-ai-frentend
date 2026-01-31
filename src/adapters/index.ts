992fb3d90efc73aa55e65b49620c2f2faaa4564b
bannysukumar <bannysukumar@gmail.com>
2026-02-01 00:13:33 +0530

    Delete src directory

--- src/adapters/index.ts
+++
@@ -1,4 +0,0 @@
-export { apiAdapter } from './apiAdapter'
-export { demoAdapter } from './demoAdapter'
-export { getDataAdapter, hasApiBaseUrl, getApiBaseUrl } from './resolver'
-export type { DataAdapter } from './types'
